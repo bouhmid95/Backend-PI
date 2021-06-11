98f230458feee137123ab6f548c86ea7e84a42ba
Ahmed Karoui <karouii.ahmed@gmail.com>
2021-06-11 22:20:01 +0100

    ahmed karoui initial commit

---
+++ src/main/java/esprit/com/tn/ProjetPiBackendApplication.java
@@ -0,0 +1,13 @@
+package esprit.com.tn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPiBackendApplication.class, args);
+	}
+
+}
