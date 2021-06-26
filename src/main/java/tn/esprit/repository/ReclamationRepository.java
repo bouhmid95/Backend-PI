269828131b451e7a5f1eaa1ef76b8935131abf57
Nour Ben Arfa <benarfanour9@gmail.com>
2021-06-26 16:13:39 +0200

    change status by adminstrator

--- src/main/java/tn/esprit/repository/ReclamationRepository.java
+++ src/main/java/tn/esprit/repository/ReclamationRepository.java
@@ -4,6 +4,9 @@ package tn.esprit.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -21,9 +24,14 @@ public interface ReclamationRepository extends CrudRepository<Reclamation, Integ
 	List<Object> getStatusReclamtionByDate();
 	
 	
-	List<Object> findByStatusStartsWith(String status);
+	List<Reclamation> findByStatusStartsWith(String status);
 	
 	@Query("SELECT p FROM Reclamation p WHERE p.message LIKE %:message%")
-	List<Object> searchByMessageLike(@Param("message") String message);
+	List<Reclamation> searchByMessageLike(@Param("message") String message);
+	
+	@Transactional
+	@Modifying
+	@Query("update Reclamation r set r.status=:status where r.id=:id")
+	 void updateJustStatusById(@Param("id")int id, @Param("status")String status);
 
 }
