08cf9bc3a2e60e7e41434de15a80f95dca536aa6
Nour Ben Arfa <benarfanour9@gmail.com>
2021-06-26 15:37:48 +0200

    add getWishlist number

--- src/main/java/tn/esprit/repository/WishlistRepository.java
+++ src/main/java/tn/esprit/repository/WishlistRepository.java
@@ -1,10 +1,18 @@
 package tn.esprit.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import tn.esprit.entities.Whishlist;
 
 public interface WishlistRepository extends CrudRepository<Whishlist, Integer>{
 
+	@Query(value = "SELECT COUNT(*) FROM Whishlist")
+	int getNumberWishlist();
 	
+
+
 }
