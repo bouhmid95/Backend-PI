3db9bb1a6eb972cae66bc7d2af1822ed6757f783
Nomen BEN SLIMENE <nomen@tanitweb.com>
2021-06-26 01:57:50 +0100

    Add crone Send alert out of stock to admin + decrement Stock + findStockByProduct

--- src/main/java/tn/esprit/ProjetPiBackendApplication.java
+++ src/main/java/tn/esprit/ProjetPiBackendApplication.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import tn.esprit.repository.OrderRepository;
 import tn.esprit.util.pdf.PDFGenerator;
 
 @SpringBootApplication
+@Configuration
+//@EnableScheduling
 public class ProjetPiBackendApplication implements CommandLineRunner {
 	
 	@Autowired
---
+++ src/main/java/tn/esprit/repository/OrderDetailsRepository.java
@@ -0,0 +1,10 @@
+package tn.esprit.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tn.esprit.entities.OrderDetails;
+
+public interface OrderDetailsRepository extends CrudRepository<OrderDetails, Integer> {
+	
+
+}
