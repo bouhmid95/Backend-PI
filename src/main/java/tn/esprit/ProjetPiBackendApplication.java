c834871db2c4feb9b804de740b79febdc6586cee
aymenJeddey <aymen.jeddey@esprit.tn>
2021-06-26 12:50:10 +0100

    cron

--- src/main/java/tn/esprit/ProjetPiBackendApplication.java
+++ src/main/java/tn/esprit/ProjetPiBackendApplication.java
@@ -12,7 +12,7 @@ import tn.esprit.util.pdf.PDFGenerator;
 
 @SpringBootApplication
 @Configuration
-//@EnableScheduling
+@EnableScheduling
 public class ProjetPiBackendApplication implements CommandLineRunner {
 	
 	@Autowired
--- src/main/java/tn/esprit/controller/RestControlUser.java
+++ src/main/java/tn/esprit/controller/RestControlUser.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -104,6 +105,8 @@ public class RestControlUser {
 		return user;
 	}
 
+	
+	@Scheduled(fixedDelay = 100000)
 	@GetMapping(value = "/unlockUser")
 	public void unlockUser() {
 		iUserService.unlockUser();
